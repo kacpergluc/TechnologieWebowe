642681961c67db580057469fee73e5b16bf381a4
kacpergluc <kacper.gluc@microsoft.wsei.edu.pl>
2021-05-26 14:54:53 +0200

    praca z obrazkkiem

--- Lab9/js/menu.js
+++ Lab9/js/menu.js
@@ -2,7 +2,6 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("miesko").style.marginLeft = "250px";
   }
-  
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("miesko").style.marginLeft= "0";
