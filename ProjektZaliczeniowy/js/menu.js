1f46b17ed6f23aaafaf6047cf8463243473d4230
kacpergluc <kacper.gluc@microsoft.wsei.edu.pl>
2021-06-08 00:01:56 +0200

    indext poprawiony, dodano tryby gry

---
+++ ProjektZaliczeniowy/js/menu.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("menu-nav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("menu-nav").style.width = "0";
+  }
